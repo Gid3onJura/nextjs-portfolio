57e1f305b581e14359fb0e23c7c336fccb6bea6f
Gid3onJura <homebasekorlash@googlemail.com>
2024-06-08 17:37:42 +0200

    home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,26 @@
-import Image from "next/image"
+import React from "react"
+import { FiDownload } from "react-icons/fi"
 
-export default function Home() {
-  return <main>Hallo</main>
+const Homepage = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Web-Entwickler</span>
+            <h1 className="h1">
+              Hi, I'm Gideon <br />
+              <span>Gidoen Jura</span>
+            </h1>
+          </div>
+
+          {/* photo */}
+          <div>2</div>
+        </div>
+      </div>
+    </section>
+  )
 }
+
+export default Homepage
